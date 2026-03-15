91b8169177eed1eb6fb04d85cfbff9dbeedd2de1
MdWarishh <171806296+MdWarishh@users.noreply.github.com>
2026-03-15 23:33:32 +0530

    environemt

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://khilat-backend-production.up.railway.app/api',
   imageBaseUrl: 'http://localhost:8080',
   razorpayKeyId: 'rzp_test_SPCu5m9GJKMWfF'  
 };
