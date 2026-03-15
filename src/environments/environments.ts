99043e759ee14e17437ca7c9011d45baf7ba1f23
MdWarishh <171806296+MdWarishh@users.noreply.github.com>
2026-03-16 00:10:03 +0530

    razorpay key

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://khilat-backend-production.up.railway.app/api',
   imageBaseUrl: 'http://localhost:8080',
-  razorpayKeyId: 'rzp_test_SPCu5m9GJKMWfF'  
+  // razorpayKeyId: 'rzp_test_SPCu5m9GJKMWfF'  
+  razorpayKeyId: 'rzp_live_SRb8YniPdmRkHt'  
 };
