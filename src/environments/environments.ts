edb757c9054b640c9491958332dcf00b59ef7e0a
MdWarishh <171806296+MdWarishh@users.noreply.github.com>
2026-03-15 21:40:24 +0530

    fix angular budget

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
   imageBaseUrl: 'http://localhost:8080',
-  razorpayKeyId: 'rzp_test_SPCu5m9GJKMWfF'  // tera actual key
+  razorpayKeyId: 'rzp_test_SPCu5m9GJKMWfF'  
 };
